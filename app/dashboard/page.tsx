58a26a500d721f47f8ce590380e492559e124e3f
damik <damianbostan12@gmail.com>
2024-05-26 15:42:29 +0300

    big tonka

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react'
+import Button from '../components/ui/Button'
+
+interface pageProps {
+
+}
+
+const Dashboard:FC<pageProps> = ({}) => {
+  return (
+    <div>
+        <Button>bruh</Button>
+    </div>
+  )
+}
+
+export default Dashboard;
