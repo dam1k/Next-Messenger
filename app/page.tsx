caca5416adaeb14d8d7a2d51d4bd8679591cf05e
damik <damianbostan12@gmail.com>
2024-05-26 17:11:26 +0300

    added auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import {db} from "./lib/db";
+
+export default async function Home() {
+
+  await db.set('hello', 'hello');
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
